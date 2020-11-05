f575f2b46714237d2b5bbce5e2d9e1ae0439a8ca
Jeison Barbosa <jeison.barbosa@ceiba.com.co>
2020-11-05 10:59:06 -0500

    Gral: Componente input de prueba

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
         <px-assets-table v-bind:assets="assets" />
+        <px-input></px-input>
     </div>
 </template>
 
 <script>
 import PxAssetsTable from '@/components/PxAssetsTable'
+import PxInput from '@/components/PxInput'
 import api from '@/api'
 
 export default {
     name: 'Home',
-    components: { PxAssetsTable },
+    components: { PxAssetsTable, PxInput },
     data() {
         return {
             assets: []
